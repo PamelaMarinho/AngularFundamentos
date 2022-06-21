802ff2751f4cbf6f14a6408da7d79971d4538910
pamelamarinho <pamelamarinhopm@hotmail.com>
2022-06-21 17:40:58 -0300

    event binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -3,17 +3,38 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
-})
+  styles: [
+    `
+   .cor{
+        background-color: aqua;
+        font-weight: bold;
+      }
+  `
+  ]
+})  
+
 export class DataBindingComponent implements OnInit {
 
   info: string;
+  valorAtual: any;
+  url= "http://lorempixel.com.br/200/100/?1";
+  mouseOver: boolean = true;
+ 
+  mouseOutOver(){
+    this.mouseOver = !this.mouseOver;
+  }
 
   getValor(){
     return 1
   }
 
-  url= "http://lorempixel.com.br/200/100/?1"
+  altera(){
+    console.log('Funfou')
+  }
+
+  pressionado(evento:any){
+    this.valorAtual = (<HTMLInputElement>(evento.target)).value
+  }
 
   constructor() {
 
